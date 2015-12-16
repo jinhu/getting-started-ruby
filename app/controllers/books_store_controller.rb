17314e232692f4cbd8c7752edbaab749a2b0f8e2
jin <jinmin.hu@altran.com>
2015-12-16 23:32:41 +0100

    modify controller

--- app/controllers/books_store_controller.rb
+++ app/controllers/books_store_controller.rb
@@ -11,38 +11,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# [START index]
 class BooksController < ApplicationController
 
   PER_PAGE = 10
 
   def index
-    page = params[:more] ? params[:more].to_i : 0
-
-    #@books = Book.limit(PER_PAGE).offset PER_PAGE * page
-    #@more  = page + 1 if @books.count == PER_PAGE
-
-    @books, @more = Book.query limit: PER_PAGE, cursor: params[:more]
-
+    @books, @cursor = Book.query limit: PER_PAGE, cursor: params[:cursor]
   end
+# [END index]
 
   def new
     @book = Book.new
   end
 
+  # [START show]
+  def show
+    @book = Book.find params[:id]
+  end
+  # [END show]
+
   def edit
     @book = Book.find params[:id]
   end
 
-  def show
-    @book = Book.find params[:id]
-  end
-
-  def destroy
-    @book = Book.find params[:id]
-    @book.destroy
-    redirect_to books_path
-  end
-
+  # [START update]
   def update
     @book = Book.find params[:id]
 
@@ -53,7 +46,19 @@ class BooksController < ApplicationController
       render :edit
     end
   end
+  # [END update]
 
+  # [START destroy]
+  def destroy
+    @book = Book.find params[:id]
+    @book.destroy
+    redirect_to books_path
+  end
+  # [END destroy]
+
+  before_filter :convert_published_on_to_date
+
+  # [START create]
   def create
     @book = Book.new book_params
 
@@ -68,8 +73,14 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit :title, :author, :published_on, :description,
-                                 :cover_image
+    params.require(:book).permit(:title, :author, :published_on, :description)
+  end
+  # [END create]
+
+  def convert_published_on_to_date
+    if params[:book] && params[:book][:published_on].present?
+      params[:book][:published_on] = Time.parse params[:book][:published_on]
+    end
   end
 
 end
