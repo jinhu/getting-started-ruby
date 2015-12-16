9a694de9349f1255752133da0ff41586223d1b47
jin <jinmin.hu@altran.com>
2015-12-16 23:22:52 +0100

    combine with 2

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,15 +12,131 @@
 # limitations under the License.
 
 # [START book]
-class Book < ActiveRecord::Base
-  validates :title, presence: true
+class Book
 
-  attr_accessor :cover_image
+  attr_accessor :cover_image, :id, :title, :author, :published_on, :description
+  # Return a Gcloud::Datastore::Dataset for the configured dataset.
+  # The dataset is used to create, read, update, and delete entity objects.
+  def self.dataset
+    @dataset ||= Gcloud.datastore(
+        Rails.application.config.database_configuration[Rails.env]["dataset_id"]
+    )
+  end
+  # [END book_class]
+
+  # [START query]
+  # Query Book entities from Cloud Datastore.
+  #
+  # returns an array of Book query results and a cursor
+  # that can be used to query for additional results.
+  def self.query options = {}
+    query = Gcloud::Datastore::Query.new
+    query.kind "Book"
+    query.limit options[:limit]   if options[:limit]
+    query.cursor options[:cursor] if options[:cursor]
+
+    results = dataset.run query
+    books   = results.map {|entity| Book.from_entity entity }
+
+    if options[:limit] && results.size == options[:limit]
+      next_cursor = results.cursor
+    end
+
+    return books, next_cursor
+  end
+  # [END query]
+
+  # [START from_entity]
+  def self.from_entity entity
+    book = Book.new
+    book.id = entity.key.id
+    entity.properties.to_hash.each do |name, value|
+      book.send "#{name}=", value
+    end
+    book
+  end
+  # [END from_entity]
+
+  # [START find]
+  # Lookup Book by ID.  Returns Book or nil.
+  def self.find id
+    query    = Gcloud::Datastore::Key.new "Book", id.to_i
+    entities = dataset.lookup query
+
+    from_entity entities.first if entities.any?
+  end
+  # [END find]
+
+  # Add Active Model support.
+  # Provides constructor that takes a Hash of attribute values.
+  include ActiveModel::Model
+
+  # [START save]
+  # Save the book to Datastore.
+  # @return true if valid and saved successfully, otherwise false.
+  def save
+    if valid?
+      entity = to_entity
+      Book.dataset.save entity
+      self.id = entity.key.id
+      upload_image if cover_image
+
+      true
+    else
+      false
+    end
+  end
+  # [END save]
+
+  # [START to_entity]
+  # ...
+  def to_entity
+    entity = Gcloud::Datastore::Entity.new
+    entity.key = Gcloud::Datastore::Key.new "Book", id
+    entity["title"]        = title
+    entity["author"]       = author       if author
+    entity["published_on"] = published_on if published_on
+    entity["description"]  = description  if description
+    entity
+  end
+  # [END to_entity]
+
+  # [START validations]
+  # Add Active Model validation support to Book class.
+  include ActiveModel::Validations
+  validates :title, presence: true
+  # [END validations]
+
+  # [START update]
+  # Set attribute values from provided Hash and save to Datastore.
+  def update attributes
+
+    attributes.each do |name, value|
+      send "#{name}=", value if respond_to? "#{name}="
+    end
+    update_image if cover_image
+    save
+  end
+  # [END update]
+
+  # [START destroy]
+  def destroy
+    delete_image if image_url
+
+    Book.dataset.delete Gcloud::Datastore::Key.new "Book", id
+  end
+  # [END destroy]
+
+  ##################
+
+  def persisted?
+    id.present?
+  end
 
   private
 
   # [START upload]
-  after_create :upload_image, if: :cover_image
+  # after_create :upload_image, if: :cover_image
 
   def upload_image
     image = StorageBucket.files.new(
@@ -38,7 +154,7 @@ class Book < ActiveRecord::Base
   # TODO what if the image is from the Pub/Sub job and NOT in Cloud Storage!?
 
   # [START delete]
-  before_destroy :delete_image, if: :image_url
+  #before_destroy :delete_image, if: :image_url
 
   def delete_image
     bucket_name = StorageBucket.key
@@ -56,7 +172,7 @@ class Book < ActiveRecord::Base
   # [END delete]
 
   # [START update]
-  before_update :update_image, if: :cover_image
+  #before_update :update_image, if: :cover_image
 
   def update_image
     delete_image if image_url?
